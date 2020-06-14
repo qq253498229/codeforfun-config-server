1c63dc44ed0fab39ced19341c022ea6fcac1e463
大胃王 <253498229@qq.com>
2020-06-14 23:08:46 +0800

    update

--- src/main/java/cn/codeforfun/generator/mapper/AppMapper.java
+++ src/main/java/cn/codeforfun/generator/mapper/AppMapper.java
@@ -1,7 +1,11 @@
 package cn.codeforfun.generator.mapper;
 
 import cn.codeforfun.generator.model.App;
+import cn.codeforfun.modules.app.vo.AppVO;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.List;
+
 public interface AppMapper extends Mapper<App> {
+    List<AppVO> findAppList();
 }
--- src/main/java/cn/codeforfun/generator/mapper/EnvMapper.java
+++ src/main/java/cn/codeforfun/generator/mapper/EnvMapper.java
@@ -3,5 +3,8 @@ package cn.codeforfun.generator.mapper;
 import cn.codeforfun.generator.model.Env;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.List;
+
 public interface EnvMapper extends Mapper<Env> {
+    List<Env> findEnvList(Long projectId);
 }
