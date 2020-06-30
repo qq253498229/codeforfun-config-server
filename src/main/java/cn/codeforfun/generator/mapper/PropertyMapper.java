5cafab068b4f2092b53ae98bf1fdf8838dad353c
大胃王 <253498229@qq.com>
2020-06-30 09:35:02 +0800

    update

--- src/main/java/cn/codeforfun/generator/mapper/PropertyMapper.java
+++ src/main/java/cn/codeforfun/generator/mapper/PropertyMapper.java
@@ -8,4 +8,6 @@ import java.util.List;
 
 public interface PropertyMapper extends Mapper<Property> {
     List<Property> find(@Param("projectName") String projectName, @Param("appName") String appName, @Param("profiles") String[] profiles);
+
+    void deleteByConfigId(@Param("configId") Long configId);
 }
